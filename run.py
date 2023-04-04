663ee35180a0b52b97c9187bb449d03843430424
Riley Hales <rileyhales1@gmail.com>
2023-04-04 12:47:02 -0600

    add print statements and fix glob patterns for ec2

--- run.py
+++ run.py
@@ -20,19 +20,25 @@ if __name__ == '__main__':
 
     tdx_header_number = 10
     for streams_gpkg, basins_gpkg in zip(
-            sorted(glob.glob(f'/tdxhydro/tdxhydro_streamnet*.gpkg')),
-            sorted(glob.glob(f'/tdxhydro/tdxhydro_streamreach_basins*.gpkg'))
+            sorted(glob.glob(f'/tdxhydro/TDX_streamnet*.gpkg')),
+            sorted(glob.glob(f'/tdxhydro/TDX_streamreach_basins*.gpkg'))
     ):
         region_number = os.path.basename(streams_gpkg).split('_')[2]
 
         out_dir = os.path.join(rapid_outputs, region_number)
         if not os.path.exists(out_dir):
             os.makedirs(out_dir)
+        if not os.path.exists(os.path.join(gpkg_outputs, region_number)):
+            os.makedirs(os.path.join(gpkg_outputs, region_number))
+
+        print(streams_gpkg)
+        print(basins_gpkg)
+        print(region_number)
+        print(out_dir)
+        print(os.path.join(gpkg_outputs, region_number))
 
         PreprocessForRAPID(streams_gpkg, basins_gpkg, sample_grids, out_dir)
 
         # move the gpkg files to a separate directory
-        if not os.path.exists(os.path.join(gpkg_outputs, region_number)):
-            os.makedirs(os.path.join(gpkg_outputs, region_number))
         for f in glob.glob(os.path.join(out_dir, '*.gpkg')):
             os.rename(f, os.path.join(gpkg_outputs, region_number, os.path.basename(f)))
