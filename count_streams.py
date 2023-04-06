cdf39f289d9086d097767774b504e5e8992031c4
Riley Hales <rileyhales1@gmail.com>
2023-04-05 18:59:33 -0600

    save counted streams

--- count_streams.py
+++ count_streams.py
@@ -7,11 +7,11 @@ import numpy as np
 
 
 def count_streams(gpkg):
-    return os.path.basename(gpkg).split('_')[2], gpd.read_file(gpkg, geometry=False).shape[0]
+    return os.path.basename(gpkg).split('_')[2], gpd.read_file(gpkg, ignore_geometry=True).shape[0]
 
 
 if __name__ == '__main__':
-    with Pool(6) as p:
+    with Pool(12) as p:
         results = p.map(count_streams, sorted(glob.glob('/Users/rchales/Data/TDXHydro/TDX_streamnet*')))
     results = np.array(results)
-    pd.DataFrame(results, columns=['region', 'count']).to_csv('stream_counts.csv', index=False)
+    pd.DataFrame(results, columns=['region', 'count']).to_csv('network_data/stream_counts.csv', index=False)
