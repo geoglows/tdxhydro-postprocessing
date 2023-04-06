38454cf0182332c4404481f9924c3b04d3f0870b
Riley Hales <rileyhales1@gmail.com>
2023-04-05 18:49:14 -0600

    count streams script

---
+++ count_streams.py
@@ -0,0 +1,17 @@
+import os
+import glob
+import pandas as pd
+import geopandas as gpd
+from multiprocessing import Pool
+import numpy as np
+
+
+def count_streams(gpkg):
+    return os.path.basename(gpkg).split('_')[2], gpd.read_file(gpkg, geometry=False).shape[0]
+
+
+if __name__ == '__main__':
+    with Pool(6) as p:
+        results = p.map(count_streams, sorted(glob.glob('/Users/rchales/Data/TDXHydro/TDX_streamnet*')))
+    results = np.array(results)
+    pd.DataFrame(results, columns=['region', 'count']).to_csv('stream_counts.csv', index=False)
